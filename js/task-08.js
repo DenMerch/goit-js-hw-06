08c2cb6f94c8ba76bf3b69bc1d8e2dd2039e8111
DenMerch <rosignolli@gmail.com>
2022-12-25 20:08:15 +0200

    done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handSubmit);
+function handSubmit(event) {
+    event.preventDefault();
+    const { elements: { email, password
 
+    } } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return console.log("Please fill in all the fields!");
+    }
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const color = document.querySelector(".color");
+btn.addEventListener("click", changeColor => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+})
