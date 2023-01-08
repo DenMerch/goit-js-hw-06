3294784dc3c7492a6fcdd3dd5a919a7ab8c64a79
DenMerch <rosignolli@gmail.com>
2023-01-08 16:48:39 +0200

    first done

--- js/task-10.js
+++ js/task-10.js
@@ -1,20 +1,20 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-// const input = document.querySelector("input");
-// const createBtn = document.querySelector("[data-create]");
-// const destroyBtn = document.querySelector("[data-destroy]");
-// const container = document.querySelector("#boxes");
-// input.addEventListener("change", () => {
-//   return input.value;
-// });
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const container = document.querySelector("#boxes");
+input.addEventListener("change", () => {
+  return input.value;
+});
 
-// createBtn.addEventListener("click", () => {
-//   for (let i = 0; i < input.value; i++) {
-//     container.insertAdjacentHTML("beforeend",
-//       `<div style = "width:${30 + 10 * i}px; height:${30 + 10 * i}px; background-color:${getRandomHexColor()}"></div>`);
-//   }
-// });
-// destroyBtn.addEventListener("click", () => {
-//   container.innerHTML = "";
-// })
+createBtn.addEventListener("click", () => {
+  for (let i = 0; i < input.value; i++) {
+    container.insertAdjacentHTML("beforeend",
+      `<div style = "width:${30 + 10 * i}px; height:${30 + 10 * i}px; background-color:${getRandomHexColor()}"></div>`);
+  }
+});
+destroyBtn.addEventListener("click", () => {
+  container.innerHTML = "";
+})
